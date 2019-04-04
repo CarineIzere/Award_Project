7848c5418dbd25126ce7ecffb4735724b4cab898
CarineIzere <carizeree@gmail.com>
2019-04-04 12:46:32 +0200

    remove adminsite.register, rate, profile

--- award/admin.py
+++ award/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Project,Profile,Rating
 
 # admin.site.register(Project)
-admin.site.register(Profile)
-admin.site.register(Rating)
+# admin.site.register(Profile)
+# admin.site.register(Rating)
